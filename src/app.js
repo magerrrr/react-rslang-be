94157b428fa10893d94b47e2a6ce31e72f425bc0
Kirill Mager <hi.kirill.m@gmail.com>
2021-03-28 18:50:07 +0300

    feat(App.js - express - json): add maximum json size limitation to 4mb

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '4mb' }));
 
 app.use('/files', express.static(path.join(__dirname, '../files')));
 
